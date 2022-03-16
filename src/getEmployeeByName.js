d7d4a02bc8e2421e9ab49b4025fba424816523e5
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-16 18:12:19 -0300

    Task 3 done - 30%

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const getEmployee = employees.find((human) =>
+    human.firstName === employeeName || human.lastName === employeeName);
+  return getEmployee;
 }
+
+console.log(getEmployeeByName('Strauss'));
 
 module.exports = getEmployeeByName;
