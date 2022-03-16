74bf8e6a57c7d0623272530d708eec0fa52b3fb3
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-16 17:36:53 -0300

    Task 2 done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+const { species } = data;
+
+function getAnimalsOlderThan(NameAnimal, age) {
+  const wantedAnimal = species.find((animal) => animal.name === NameAnimal);
+  const { residents } = wantedAnimal;
+  return residents.every((inhabitant) => inhabitant.age > age);
 }
+
+// console.log(getAnimalsOlderThan('frogs', 1));
 
 module.exports = getAnimalsOlderThan;
