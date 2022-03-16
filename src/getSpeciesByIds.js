218db820c8d85b82d765bf4f20ea9a9f317fc3d1
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-16 16:23:41 -0300

    1.Task done, test local ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código here
+// console.log(data);
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  const wantedSpecies = [];
+  ids.forEach((id) => {
+    wantedSpecies.push(species.find((animal) => animal.id === id));
+  });
+  return wantedSpecies;
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
