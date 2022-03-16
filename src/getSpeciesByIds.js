5854b5744046820e083891e1e647662a4a22a597
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-16 16:25:48 -0300

    1.Task console.log commented out

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -10,6 +10,6 @@ function getSpeciesByIds(...ids) {
   });
   return wantedSpecies;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
