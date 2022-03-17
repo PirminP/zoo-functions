79d061179fba415a5be22fad295ae3b5ceb00780
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-17 18:33:36 -0300

    Task 7 not done-half way

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+function getResidents(i, sex, sorted) {
+  let animalName = species[i].residents.map((animal) => animal.name);
+  if (sex === 'male' || sex === 'female') {
+    animalName = species[i].residents
+      .filter((animal) => animal.sex === sex)
+      .map((animal) => animal.name);
+  }
+  if (sorted === true) {
+    animalName.sort();
+  }
+  return animalName;
+}
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sex, sorted } = options;
+  const animalMap = { NE: [], NW: [], SE: [], SW: [] };
+  if (includeNames === undefined) {
+    species.forEach((specie) => {
+      animalMap[specie.location].push(species.name);
+    });
+    return animalMap;
+  }
+  species.forEach((specie, i) => {
+    animalMap[specie.location].push({ [specie.name]: getResidents(i, sex, sorted) });
+  });
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
