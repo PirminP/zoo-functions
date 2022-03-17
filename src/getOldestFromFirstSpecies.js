2f47cf8f6ad457d472b7ad2d2e67619b610cfa5e
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-17 19:07:38 -0300

    Task 9 done - one missing

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = employees.find((employee) => employee.id === id);
+  const idFirstSpecie = getEmployee.responsibleFor[0];
+  const firstSpecie = species.find((specie) => specie.id === idFirstSpecie);
+  const specieResidents = firstSpecie.residents;
+  const oldestSpecie = specieResidents.reduce((acc, specie) =>
+    (acc.age > specie.age ? acc : specie));
+  return Object.values(oldestSpecie);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
