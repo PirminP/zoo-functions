f8584529dc6f557c036f5cd7f5a8eb0c97fa2d0b
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-17 13:08:14 -0300

    Task 5 done - half way

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const allAnimals = () => {
+  const obj = {};
+  species.forEach((eachSpecies) => {
+    obj[eachSpecies.name] = eachSpecies.residents.length;
+  });
+  return obj;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return allAnimals();
+  }
+
+  const chosenSpecie = species.find((specie) => specie.name === animal.specie);
+  const { residents } = chosenSpecie;
+
+  if (animal.sex) {
+    const genderSpecies = residents.filter((eachSpecies) =>
+      eachSpecies.sex === animal.sex);
+    return genderSpecies.length; // return quantity of gender found in array
+  }
+
+  return residents.length; // return quantity of species found in object array
 }
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'snakes' }));
+console.log(countAnimals({ specie: 'snakes', sex: 'male' }));
 
 module.exports = countAnimals;
