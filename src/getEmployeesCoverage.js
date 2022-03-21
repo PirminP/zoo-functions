4d66d0c7753b77c209a6b218d419978edc7be37d
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-21 13:30:17 -0300

    Task 10 done - 100% of project

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function getAllEmployee() {
+  const entireEmployees = employees.map((employee) => {
+    const newObj = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: employee.responsibleFor.map((id) => species
+        .find((animal) => animal.id === id).name),
+      locations: employee.responsibleFor.map((id) => species
+        .find((animal) => animal.id === id).location),
+    };
+    return newObj;
+  });
+  return entireEmployees;
+}
+
+function getOneEmployee(nameOrId) {
+  const currentEmployee = employees
+    .find((employee) => employee.firstName === nameOrId
+    || employee.lastName === nameOrId || employee.id === nameOrId);
+  if (!currentEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  const newObj = {
+    id: currentEmployee.id,
+    fullName: `${currentEmployee.firstName} ${currentEmployee.lastName}`,
+    species: currentEmployee.responsibleFor.map((id) => species
+      .find((animal) => animal.id === id).name),
+    locations: currentEmployee.responsibleFor.map((id) => species
+      .find((animal) => animal.id === id).location),
+  };
+  return newObj;
+}
+
+function getEmployeesCoverage(chosenEmployee) {
+  if (!chosenEmployee) {
+    return getAllEmployee();
+  }
+  if (chosenEmployee.id) {
+    return getOneEmployee(chosenEmployee.id);
+  }
+  if (chosenEmployee.name) {
+    return getOneEmployee(chosenEmployee.name);
+  }
 }
 
 module.exports = getEmployeesCoverage;
